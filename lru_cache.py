edce98f81d791f8ae7c1ad44dae415fdf7abd06f
acmerfight <acmerfight@gmail.com>
2014-04-15 22:54:56 +0800

    add feature: add basic lru cache

---
+++ lru_cache.py
@@ -0,0 +1,65 @@
+#coding=utf-8
+
+import time
+from threading import RLock
+from collections import OrderedDict
+
+
+class LruCache(object):
+    lock = RLock()
+    cache_size = 0
+
+    def __init__(self, maxsize=100, timeout=3600):
+        self.maxsize = maxsize
+        self.timeout = timeout
+        self.cache = OrderedDict()
+        self.is_full = False
+
+    def __call__(self, func):
+
+        def wrapper(*args, **kwds):
+            key = self.make_key(args, kwds)
+            with self.lock:
+                result_tuple = self.cache.get(key)
+                if result_tuple is not None:
+                    result, old_time = result_tuple
+                    if int(time.time()) - old_time <= self.timeout:
+                        del self.cache[key]
+                        self.cache[key] = result_tuple
+                        return result
+                    else:
+                        del self.cache[key]
+                        self.is_full = (len(self.cache) > self.maxsize) 
+            result_tuple = func(*args, **kwds), int(time.time())
+            with self.lock:
+                if key in self.cache:
+                    pass
+                elif self.is_full:
+                    self.cache.popitem(last=False)
+                    self.cache[key] = result_tuple
+                else:
+                    self.cache[key] = result_tuple
+                    self.is_full = (len(self.cache) > self.maxsize) 
+            print self.cache
+            return result_tuple[0]
+        return wrapper
+
+    @staticmethod
+    def make_key(args, kwds):
+        key = args
+        if kwds:
+            sorted_items = sorted(kwds.items())
+            for item in sorted_items:
+                key += item
+        return key
+
+
+@LruCache(maxsize=2, timeout=3)
+def foo(a):
+    print "into"
+    return a
+
+@LruCache(maxsize=2)
+def bar(a):
+    print "into"
+    return a + 1
