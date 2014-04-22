6d45bdda92ae84b2403d3058cc63deaf9564c1b4
acmerfight <acmerfight@gmail.com>
2014-04-22 23:42:31 +0800

    add feature: cache when maxsize and timeout is None

--- lru_cache.py
+++ lru_cache.py
@@ -12,37 +12,48 @@ class LruCache(object):
     def __init__(self, maxsize=100, timeout=3600):
         self.maxsize = maxsize
         self.timeout = timeout
-        self.cache = OrderedDict()
-        self.is_full = False
-        self.lock = RLock()
         self.Omitted_object = OmittedType()
+        if maxsize is not None and timeout is not None:
+            self.cache = OrderedDict()
+            self.is_full = False
+            self.lock = RLock()
+        elif maxsize is None and timeout is None:
+            self.cache = {}
 
     def __call__(self, func):
-
-        def wrapper(*args, **kwds):
-            key = self.make_key(args, kwds)
-            with self.lock:
-                result_tuple = self.cache.get(key, self.Omitted_object)
-                if result_tuple is not self.Omitted_object:
-                    result, old_time = result_tuple
-                    if int(time.time()) - old_time <= self.timeout:
-                        del self.cache[key]
+        if self.timeout is not None and self.maxsize is not None:
+            def wrapper(*args, **kwds):
+                key = self.make_key(args, kwds)
+                with self.lock:
+                    result_tuple = self.cache.get(key, self.Omitted_object)
+                    if result_tuple is not self.Omitted_object:
+                        result, old_time = result_tuple
+                        if int(time.time()) - old_time <= self.timeout:
+                            del self.cache[key]
+                            self.cache[key] = result_tuple
+                            return result
+                        else:
+                            del self.cache[key]
+                            self.is_full = (len(self.cache) >= self.maxsize) 
+                result_tuple = func(*args, **kwds), int(time.time())
+                with self.lock:
+                    if key in self.cache:
+                        pass
+                    elif self.is_full:
+                        self.cache.popitem(last=False)
                         self.cache[key] = result_tuple
-                        return result
                     else:
-                        del self.cache[key]
+                        self.cache[key] = result_tuple
                         self.is_full = (len(self.cache) >= self.maxsize) 
-            result_tuple = func(*args, **kwds), int(time.time())
-            with self.lock:
-                if key in self.cache:
-                    pass
-                elif self.is_full:
-                    self.cache.popitem(last=False)
-                    self.cache[key] = result_tuple
-                else:
-                    self.cache[key] = result_tuple
-                    self.is_full = (len(self.cache) >= self.maxsize) 
-            return result_tuple[0]
+                return result_tuple[0]
+        elif self.timeout is None and self.maxsize is None:
+            def wrapper(*args, **kwds):
+                key = self.make_key(args, kwds)
+                result = self.cache.get(key, self.Omitted_object)
+                if result is self.Omitted_object:
+                    result = func(*args, **kwds)
+                    cache[key] = result
+                return result
         return wrapper
 
     @staticmethod
