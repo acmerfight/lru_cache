7df8ea7c7f44e98ca5dc75747aa15efca7eae26f
acmerfight <acmerfight@gmail.com>
2014-05-06 22:15:32 +0800

    change directory

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='lru_cache',
-    version='0.1.0',
+    version='0.1.1',
     author='acmerfight',
     author_email='acmerfight@gmail.com',
     packages=['lru_cache'],
