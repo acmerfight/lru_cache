7f53aa76196b31bdf8ddce50d13cf768ccf0ba12
acmerfight <acmerfight@gmail.com>
2014-05-06 21:58:18 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='lru_cache',
+    version='0.1.0',
+    author='acmerfight',
+    author_email='acmerfight@gmail.com',
+    packages=['lru_cache'],
+    url='http://pypi.python.org/pypi/TowelStuff/',
+    license='LICENSE.txt',
+    description='thread safe lru cache',
+    long_description=open('README.md').read(),
+)
