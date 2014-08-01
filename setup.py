741987516c401d79b62d129102c4ff072d724f44
acmerfight <acmerfight@gmail.com>
2014-08-01 16:50:50 +0800

    chaneg version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='lru_cache',
-    version='0.1.5',
+    version='0.1.6',
     author='acmerfight',
     author_email='acmerfight@gmail.com',
     packages=['cache'],
