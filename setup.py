67a2d87d917cc9904bb3d91ee45f4bd7d6f0f436
acmerfight <acmerfight@gmail.com>
2014-11-24 21:44:31 +0800

    change setup

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='lru_cache',
-    version='0.1.7',
+    version='0.1.8',
     author='acmerfight',
     author_email='acmerfight@gmail.com',
-    packages=['cache'],
+    packages=['lru_cache'],
     url='https://github.com/acmerfight/lru_cache',
-    license='GPL',
+    license='MIT',
     description='thread safe lru cache',
 )
