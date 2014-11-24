69a1a1497561c475b407f91a9d8f50694b55ff4b
acmerfight <acmerfight@gmail.com>
2014-11-24 22:12:08 +0800

    change setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='lru_cache',
-    version='0.1.8',
+    version='0.1.9',
     author='acmerfight',
     author_email='acmerfight@gmail.com',
     packages=['lru_cache'],
